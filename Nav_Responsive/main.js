a855a5428ae0edc8317899419236de7b09f3fc38
unknown <jonnathan.rodriguez@cun.edu.co>
2023-02-03 09:50:44 -0500

    Inicio del diseño de Card coa-cola

--- Nav_Responsive/main.js
+++ Nav_Responsive/main.js
@@ -1,15 +1,28 @@
-let searchButton = document.querySelector('.search_button');
-let closeButton = document.querySelector('.close_button');
-let searchBox = document.querySelector('.search_box');
+let searchButton = document.querySelector(".search_button");
+let closeButton = document.querySelector(".close_button");
+let searchBox = document.querySelector(".search_box");
+let navigation = document.querySelector(".navigation");
+let menoToggle = document.querySelector(".menu_toggle");
+let header = document.querySelector("header");
 
-searchButton.onclick = () =>{
-    searchBox.classList.add('active');
-    searchButton.classList.add('active');
-    closeButton.classList.add('active');
-}
+searchButton.onclick = () => {
+  searchBox.classList.add("active");
+  searchButton.classList.add("active");
+  closeButton.classList.add("active");
+  menoToggle.classList.add("hide");
+  header.classList.remove("open");
+};
 
-closeButton.onclick =  () =>{
-    searchBox.classList.remove('active')
-    searchButton.classList.remove('active')
-    closeButton.classList.remove('active')
-}
+closeButton.onclick = () => {
+  searchBox.classList.remove("active");
+  searchButton.classList.remove("active");
+  closeButton.classList.remove("active");
+  menoToggle.classList.remove("hide");
+};
+
+menoToggle.onclick = () => {
+  header.classList.toggle("open");
+  searchBox.classList.remove("active");
+  searchButton.classList.remove("active");
+  closeButton.classList.remove("active");
+};
